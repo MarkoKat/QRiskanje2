5cb8c2307e6b22a714b3f35a6a5b759898082c39
MarkoKat <marko.katrasnik@gmail.com>
2018-06-03 00:40:07 +0200

    Ociscena koda + strings

--- app/src/main/java/uni/fe/tnuv/qrtest5/MapsActivity.java
+++ app/src/main/java/uni/fe/tnuv/qrtest5/MapsActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
@@ -62,6 +63,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        LinearLayout pickActivity = (LinearLayout) findViewById(R.id.pick_activity);
+        pickActivity.bringToFront();
 
         filename = getResources().getString(R.string.datotekaZVsebino);
         filenameUser = getResources().getString(R.string.datotekaZVsebinoUser);
